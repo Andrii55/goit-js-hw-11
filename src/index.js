b6cf95fe34da59554f157e42af70f92b249b8950
Andrii55 <117086425+Andrii55@users.noreply.github.com>
2023-04-22 12:37:55 +0300

    addCssAndHTTP

--- src/index.js
+++ src/index.js
@@ -1,25 +1,118 @@
-import axios from 'axios';
 import Notiflix from 'notiflix';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
-<link rel="stylesheet" href="node_modules/simplelightbox/dist/simple-lightbox.min.css"></link>
+import axios from 'axios';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 
-// Прокручування сторінки
-// const { height: cardHeight } = document
-//   .querySelector(".gallery")
-//   .firstElementChild.getBoundingClientRect();
 
-// window.scrollBy({
-//   top: cardHeight * 2,
-//   behavior: "smooth",
-// });
+const ref = {
+  searchFormEl: document.querySelector('.search-form'),
+  galleryEl: document.querySelector('.gallery'),
+  searchButtonEl: document.querySelector('button[type="submit"]'),
+  searchInputEl: document.querySelector('input[name="searchQuery"]'),
+};
 
-// Нескінченний скрол
-// Замість кнопки «Load more», можна зробити нескінченне завантаження зображень під час прокручування сторінки. 
-// Ми надаємо тобі повну свободу дій в реалізації, можеш використовувати будь-які бібліотеки.
+ref.searchFormEl.addEventListener('submit', onSearchSubmit);
 
-// Повідомлення
-// Після першого запиту з кожним новим пошуком отримувати повідомлення, 
-// в якому буде написано, скільки всього знайшли зображень (властивість totalHits). 
-// Текст повідомлення - "Hooray! We found totalHits images."
+function onSearchSubmit(e) {
+  e.preventDefault();
+  onSearchClick();
+}
+
+async function onSearchClick() {
+  const searchQuery = ref.searchInputEl.value.trim();
+  if (!searchQuery) {
+    return;
+  }
+
+  const KEY = '35605162-423c41836779f9af24399a6b4';
+  const imageType = 'photo';
+  const orientation = 'horizontal';
+  const safeSearch = 'true';
+  const perPage = 40;
+  const url = `https://pixabay.com/api/?key=${KEY}&q=${encodeURIComponent(searchQuery)}&image_type=${imageType}&orientation=${orientation}&safesearch=${safeSearch}&per_page=${perPage}`;
+
+  ref.galleryEl.innerHTML = '';
+
+  try {
+    const response = await axios.get(url);
+    console.log(url)
+    const images = response.data.hits.map(hit => ({
+      webformatURL: hit.webformatURL,
+      largeImageURL: hit.largeImageURL,
+      tags: hit.tags,
+      likes: hit.likes,
+      views: hit.views,
+      comments: hit.comments,
+      downloads: hit.downloads,
+    }));
+    if (images.length === 0) {
+      Notiflix.Notify.info(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      return;
+    }
+
+    renderGallery(images);
+  } catch (error) {
+    console.error(error);
+    Notiflix.Notify.failure('Something went wrong. Please try again later.');
+  }
+}
+
+function renderGallery(images) {
+  const galleryMarkup = images
+    .map(image => {
+      const {
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      } = image;
+      return (`
+        <div class="photo-card">
+          {' '}
+          <img
+            src="${webformatURL}"
+            alt="${tags}"
+            loading="lazy"
+            data-source="${largeImageURL}"
+          />{' '}
+          <div class="info">
+            {' '}
+            <p class="info-item">
+              {' '}
+              <b>Likes:</b> ${likes}{' '}
+            </p>{' '}
+            <p class="info-item">
+              {' '}
+              <b>Views:</b> ${views}{' '}
+            </p>{' '}
+            <p class="info-item">
+              {' '}
+              <b>Comments:</b> ${comments}{' '}
+            </p>{' '}
+            <p class="info-item">
+              {' '}
+              <b>Downloads:</b> ${downloads}{' '}
+            </p>{' '}
+          </div>{' '}
+        </div>`
+      );
+    })
+    .join('');
+
+  ref.galleryEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+  const galleryImagesEl = document.querySelectorAll('img');
+  galleryImagesEl.forEach(image => {
+    image.addEventListener('click', onImageClick);
+  });
+}
+
+// function onImageClick(e) {
+//   const largeImageUrl = e.target.dataset.source;
+// }
